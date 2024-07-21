6cdd0e322481c654d3357e12503e3042d8ad6807
Mariusz Kalinowski <Mariusz.Kalinowski95@gmail.com>
2024-07-21 10:15:22 +0200

    added curriencies to  file

---
+++ src/Form/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.35,
+    },
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 3.95,
+    },
+    {   
+        short: "GBP",
+        name: "Funt brytyjski",
+        rate: 5.05,
+    },
+];
