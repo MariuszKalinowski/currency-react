eb6180594489db3762dc44b58d4f36d45cebde27
Mariusz Kalinowski <Mariusz.Kalinowski95@gmail.com>
2024-07-23 20:44:30 +0200

    added the result display

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -1,7 +1,10 @@
 import "./style.css"
 
-const Result = () => (
+const Result = ({ result, amount, selectedCurrency, forResult }) => (
     <div className="form__result">
+
+        {result !== null ? <p>{amount}PLN = {result.toFixed(2)} {selectedCurrency.short} </p> : null}
+        
 
     </div>
 
