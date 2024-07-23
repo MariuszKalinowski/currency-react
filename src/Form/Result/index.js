d8d262fa131bb613eec942a9ca0e0643229ae484
Mariusz Kalinowski <Mariusz.Kalinowski95@gmail.com>
2024-07-23 20:45:19 +0200

    added strong to result

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -3,8 +3,8 @@ import "./style.css"
 const Result = ({ result, amount, selectedCurrency, forResult }) => (
     <div className="form__result">
 
-        {result !== null ? <p>{amount}PLN = {result.toFixed(2)} {selectedCurrency.short} </p> : null}
-        
+        {result !== null ? <p>{amount} PLN = <strong>{result.toFixed(2)} {selectedCurrency.short}</strong> </p> : null}
+
 
     </div>
 
