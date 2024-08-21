d5015cc264926c36fc0fee550ac77e48fcc55cfc
Mariusz Kalinowski <Mariusz.Kalinowski95@gmail.com>
2024-08-21 21:42:38 +0200

    removed className

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -4,7 +4,7 @@ const Result = ({ result }) => (
     <div className="form__result">
 
         {result ? (
-            <p>
+            <p className="form__resultText">
                 {result.amount} PLN = <strong>
                     {result.result.toFixed(2)} {result.toCurrency}</strong>
             </p>
